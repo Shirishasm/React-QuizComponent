90f0f5c43d6836c9fba53e66ee3dd374f36640a4
Shirishasm <47881984+Shirishasm@users.noreply.github.com>
2019-03-27 16:33:04 +0530

    update9th

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react'
 
-let quiz_data = require('./quiz_data.json')
+let quizData = require('./quiz_data.json')
 
  class Quiz extends Component{
     constructor(props){
