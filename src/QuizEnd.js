41fce90e8af128eff238f12c6454ec5f4ea1fc27
Shirishasm <47881984+Shirishasm@users.noreply.github.com>
2019-03-28 16:05:48 +0530

    new4

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,7 +3,12 @@ import React,{Component} from 'react'
 class QuizEnd extends Component{
     
   reder(){
-        return 
+        return (
+          <div>
+        <p>Thanks for playing!</p>
+        <a href=''>Reset Quiz</a>
+      </div>
+        ) 
     }
 }
 export default QuizEnd
